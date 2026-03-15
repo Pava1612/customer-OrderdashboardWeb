596f0fd014117289fd334cb7d888d2f6811502e0
pavadharanid96 <46005649-pavadharanid96@users.noreply.replit.com>
2026-03-15 17:01:39 +0000

    Initial commit

---
+++ client/src/pages/CheckboxInternal.tsx
@@ -0,0 +1,11 @@
+export const CheckboxInternal = (): JSX.Element => {
+  return (
+    <div className="relative w-4 h-4 bg-[#54bd95] rounded-[3px] flex items-center justify-center">
+      <img
+        className="w-[50%] h-[50%]"
+        alt="Vector"
+        src="/figmaAssets/vector.svg"
+      />
+    </div>
+  );
+};
